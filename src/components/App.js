664af2cc24e4e8ad39fbb7a86f6c6a7a654ed607
Koushik Das <64496945+koudee@users.noreply.github.com>
2024-03-18 22:18:02 +0000

    solution added

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
 
-import React from "react";
+import React, { Component } from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+// const App = () => {
+//   return (
+//     <div>
+//         {/* Do not remove the main div */}
+//     </div>
+//   )
+// }
+import React, { Component } from "react";
+
+class Greeting extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: "" };
+    }
+
+    onChangeText = (event) => {
+        const newValue = event.target.value;
+        this.setState({ value: newValue });
+    }
+
+    render() {
+        return (
+            <div>
+                <label htmlFor="input">Enter your name</label>
+                <input id="input" value={this.state.value} onChange={this.onChangeText} />
+                {this.state.value ? <p>Hello {this.state.value}!</p> : null}
+            </div>
+        );
+    }
 }
 
-export default App
+export default Greeting;
